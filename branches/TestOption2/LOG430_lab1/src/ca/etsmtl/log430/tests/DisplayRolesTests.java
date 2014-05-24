7b10a3a0da7f0ef14336cb502b15a8259e71e83c
SamJA <SamJA@e992412d-0ddf-459c-870d-ec51bcd183c8>
2014-05-24 05:07:35 +0000

    - Removed plugin folder not needed.

--- branches/TestOption2/LOG430_lab1/src/ca/etsmtl/log430/tests/DisplayRolesTests.java
+++ branches/TestOption2/LOG430_lab1/src/ca/etsmtl/log430/tests/DisplayRolesTests.java
@@ -1,5 +1,16 @@
 package ca.etsmtl.log430.tests;
 
-public class DisplayRolesTests {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
+public class DisplayRolesTests 
+{
+	@Test
+	public void testJUnitAssert()
+	{
+		Assert.assertTrue(true);
+	}
 }
