f09512dd26ccf22f088611a810ed23ec4e6b730c
SamJA <SamJA@e992412d-0ddf-459c-870d-ec51bcd183c8>
2014-06-01 00:42:52 +0000

    - Created presentation package - Created controller package - Created services package - Created DAO (Data Access Object ) layer  package

---
+++ branches/LayerStyle/LOG430_lab1/src/ca/etsmtl/log430/lab1/controller/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Controller Layer
+ */
+/**
+ * @author Samuel Abraham
+ * This layer should contains all classes related 
+ * to event triggered by user interaction with the application.
+ *  
+ * Controller classes should interact with Services classes by
+ * Forwarding user calls to appropriates Services functions.
+ * User should not be able to access services functions directly.  
+ */
+package ca.etsmtl.log430.lab1.controller;
