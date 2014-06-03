123904e9f67f82f283cb8347d01d4961b0a8c21e
SamJA <SamJA@e992412d-0ddf-459c-870d-ec51bcd183c8>
2014-06-03 21:23:41 +0000

    - Added lastes changed - Removed the folder ModificationIption3

---
+++ trunk/LOG430_lab1/src/ca/etsmtl/log430/lab1/DAO/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * DAO(Data Access Object) Layer 
+ */
+
+/**
+ * @author Samuel Abraham
+ * 
+ * This layer should contains all related database operations.
+ * DAO classes, handles all interactions with specific data bases
+ * 
+ * All modification related to extracting data should only been made
+ * in that layer. 
+ * Modification related to the internal functions of DAO classes 
+ * should never impact other layers's classes. 
+ */
+package ca.etsmtl.log430.lab1.DAO;
---
+++ trunk/LOG430_lab1/src/ca/etsmtl/log430/lab1/presentation/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Presentation layer 
+ */
+/**
+ * @author Samuel Abraham
+ * 
+ * This package contains should contains all
+ * related user interfaces classes. 
+ * Presentation's classes should interact with controller's classes.
+ * 
+ * Any changes related to presentation classes should only
+ * be made in this package and should not affect lower layer classes. 
+ */
+package ca.etsmtl.log430.lab1.presentation;
